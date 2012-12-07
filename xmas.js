bddeef9a00fe290cb9d2209c63a8b48841418086
Ben Edwards <benguin2kxx@gmail.com>
2012-12-06 19:26:14 -0500

    Game makes sense!

--- xmas.js
+++ xmas.js
@@ -20,15 +20,44 @@ var chimney = canvas.display.rectangle({
     fill: "#f00"
 });
 
+var score = 0;
+var scoreText = canvas.display.text({
+    x: canvas.width - 5,
+    y: 5,
+    origin: { x: "right", y: "top" },
+    font: "bold 20px sans-serif",
+    text: score,
+    fill: "#f00"
+});
+
 var present = canvas.display.rectangle({
+    x: 200,
+    y: 200,
     width: 30,
     height: 30,
     fill: '#0f0'
 });
 
+var presents = new Array();
+var presentsOnScreen = 1;
+
+canvas.bind("click", function() {
+    
+    //present.x = canvas.mouse.x - santa.radius / 2;
+    //present.y = canvas.mouse.y + santa.radius;
+    if (presents.length < presentsOnScreen) {
+        var newPresent = present.clone({
+            x: canvas.mouse.x - santa.radius / 2,
+            y: canvas.mouse.y + santa.radius
+        });
+        presents.push(newPresent);
+        canvas.addChild(newPresent);
+    }
+});
+
+canvas.addChild(scoreText);
 canvas.addChild(santa);
 canvas.addChild(chimney);
-//canvas.addChild(present);
 canvas.setLoop(function () {
     santa.x = canvas.mouse.x;
     santa.y = canvas.mouse.y;
@@ -39,13 +68,26 @@ canvas.setLoop(function () {
         chimney.moveTo(canvas.width - chimney.width, canvas.height - chimney.height);
     }
 
-});
+    if (presents.length > 0) {
+        for (var i = 0; i < presents.length; i++) {
+            if (presents[i].y <= canvas.height) {
+                presents[i].y+=2;    
+            } else {
+                canvas.removeChild(presents[i]);
+                presents.splice(i,i+1);
+            }
 
-canvas.bind("click", function() {
-    canvas.addChild(present.clone({
-        x: canvas.mouse.x - santa.radius / 2,
-        y: canvas.mouse.y + santa.radius
-    }));
+            if ((presents[i].x >= chimney.x 
+                    && presents[i].x <= chimney.x + chimney.width) 
+                    && (presents[i].y >= canvas.height - chimney.height
+                    && (presents[i].y <= canvas.height - chimney.height/2))) {
+                score += 10;
+                canvas.removeChild(presents[i]);
+                presents.splice(i,i+1);
+            }
+        }
+    }
+    scoreText.text = score;
 });
 
 canvas.timeline.start();
